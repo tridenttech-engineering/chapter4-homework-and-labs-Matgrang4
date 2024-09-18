a5c23cb945dae2a433855e4dc1a52d4c4c1d06c7
Matgrang4 <matthewgranger@my.tridenttech.edu>
2024-09-18 19:53:51 -0400

    Update hw4-11.cpp

--- hw4-11.cpp
+++ hw4-11.cpp
@@ -5,7 +5,28 @@
 using namespace std;
 
 int main() {
-  
+    // Declare variables
+    double salePrice = 0.0;
+    double taxRate = 0.0;
+    double salesTax = 0.0;
+    double cost = 0.0;
+
+    // Input the sale price
+    cout << "Enter the sale price: ";
+    cin >> salePrice;
+
+    // Input the sales tax rate
+    cout << "Enter the sales tax rate: ";
+    cin >> taxRate;
+
+    // Calculate the sales tax
+    salesTax = salePrice * taxRate;
+
+    // Calculate the total cost
+    cost = salePrice + salesTax;
+
+    // Display the total cost
+    cout << "Cost: $" << cost << endl;  
 
   return 0;
 } // end of main function
